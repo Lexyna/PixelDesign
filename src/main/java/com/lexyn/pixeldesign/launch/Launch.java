2a1967a2375f8dd95624f2da1fdfe736d9744795
Alexandra <lexieverena@outlook.de>
2019-10-10 17:50:01 +0200

    Rearanged directories

--- src/main/java/com/lexyn/pixeldesign/launch/Launch.java
+++ src/main/java/com/lexyn/pixeldesign/launch/Launch.java
@@ -4,11 +4,31 @@ import com.lexyn.pixeldesign.stages.ApplicationStage;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class Launch extends Application {
+
+    private static Logger LOGGER = Logger.getLogger(Application.class.getName());
+    private FileHandler log;
 
     @Override
     public void start(Stage primary_stage){
+
+        try{
+            log = new FileHandler("Pixellog.log", false);
+        }catch (SecurityException | IOException ex){
+            ex.printStackTrace();
+        }
+
+        Logger l = Logger.getLogger("");
+        log.setFormatter(new SimpleFormatter());
+        l.addHandler(log);
+
         new ApplicationStage().launchAppStage(primary_stage);
+
     }
 
 
--- src/main/java/com/lexyn/pixeldesign/stages/ApplicationStage.java
+++ src/main/java/com/lexyn/pixeldesign/stages/ApplicationStage.java
@@ -1,14 +1,16 @@
 package com.lexyn.pixeldesign.stages;
-
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ApplicationStage {
+
+    private static Logger logger = Logger.getLogger(ApplicationStage.class.getName());
 
     private Stage appStage;
     private Scene scene;
@@ -21,9 +23,7 @@ public class ApplicationStage {
             Parent root = loader.load();
             scene = new Scene(root, 700, 400);
         } catch (IOException ex) {
-            //TODO: -> LOGGER
-            System.out.println("Alert doesnt work --issue at github");
-            ex.printStackTrace();
+            logger.log(Level.SEVERE, ex.toString(), ex);
             System.exit(1);
         }
     }
@@ -34,7 +34,6 @@ public class ApplicationStage {
         appStage.setScene(scene);
         appStage.setTitle("PixelDesign");
         appStage.setResizable(true);
-
 
         appStage.setMinHeight(400);
         appStage.setMinWidth(700);
