ec32ceaa96b425b96621d3ee9524a9e4b3096628
Alexandra <lexieverena@outlook.de>
2019-10-14 21:32:17 +0200

    Implemented function to display Listview

--- src/main/java/com/lexyn/pixeldesign/controller/MainController.java
+++ src/main/java/com/lexyn/pixeldesign/controller/MainController.java
@@ -7,13 +7,20 @@ import com.lexyn.pixeldesign.manager.ParticleSystemManager;
 import com.lexyn.pixeldesign.render.PixelRenderer;
 import com.lexyn.pixeldesign.render.Renderer;
 import com.lexyn.pixeldesign.render.transformation.TransformationMatrix;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TitledPane;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,6 +37,13 @@ public class MainController implements Initializable {
     private Canvas fx_canvas;
     @FXML
     private TitledPane fx_canvasFrame;
+    @FXML
+    private ListView<String> fx_emitterĹist;
+
+
+    //Menu Context
+    @FXML
+    private MenuItem fx_newEmitter, fx_deleteEmitter;
 
     public MainController(){
         logger.log( Level.INFO,"Initialise Controller");
@@ -42,6 +56,23 @@ public class MainController implements Initializable {
         fx_canvas.widthProperty().bind(fx_canvasFrame.widthProperty());
         fx_canvas.heightProperty().bind(fx_canvasFrame.heightProperty().add(-10));
 
+        fx_canvasFrame.widthProperty().addListener(e -> ParticleSystemManager.getInstance().getActiveSystem().getRenderer().resize());
+        fx_canvasFrame.heightProperty().addListener(e -> ParticleSystemManager.getInstance().getActiveSystem().getRenderer().resize());
+
+        addCanvasListener(fx_canvas);
+        addMenuListener(fx_newEmitter, fx_deleteEmitter);
+
+        ParticleSystem particleSystem = new ParticleSystem(new Renderer(fx_canvas), new PixelRenderer(fx_canvas), new PixelMap(64,64, Color.web("666666", 1.0)));
+        ParticleSystemManager.getInstance().addSystem(particleSystem);
+
+    }
+
+    private void addMenuListener(MenuItem fx_newEmitter, MenuItem fx_deleteEmitter){
+        fx_newEmitter.setOnAction(e -> fx_emitterĹist.getItems().add("new Emitter"));
+        fx_deleteEmitter.setOnAction(e -> fx_emitterĹist.getItems().remove(fx_emitterĹist.getSelectionModel().getSelectedIndex()));
+    }
+
+    private void addCanvasListener(Canvas fx_canvas){
         fx_canvas.setOnMouseMoved((evt)->{
             PixelCoordinate cord = TransformationMatrix.getInstance().converToPixelCord(evt.getX(), evt.getY());
             ParticleSystemManager.getInstance().getActiveSystem().getPixelRenderer().highlightPixel(cord);
@@ -49,12 +80,6 @@ public class MainController implements Initializable {
                 fx_canvasFrame.setText("Filename + Mouse at " + cord.getX() + "/" + cord.getY());
 
         });
-
-        fx_canvasFrame.widthProperty().addListener(e -> ParticleSystemManager.getInstance().getActiveSystem().getRenderer().resize());
-        fx_canvasFrame.heightProperty().addListener(e -> ParticleSystemManager.getInstance().getActiveSystem().getRenderer().resize());
-
-        ParticleSystem particleSystem = new ParticleSystem(new Renderer(fx_canvas), new PixelRenderer(fx_canvas), new PixelMap(64,64, Color.web("666666", 1.0)));
-        ParticleSystemManager.getInstance().addSystem(particleSystem);
 
     }
 
