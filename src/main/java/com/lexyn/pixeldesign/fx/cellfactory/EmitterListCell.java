8aafb66aa40b5e383207f77595f28aa2527a8741
Alexandra <lexieverena@outlook.de>
2019-10-16 11:17:49 +0200

    Implemented drag and drop for ListView

--- src/main/java/com/lexyn/pixeldesign/fx/cellfactory/EmitterListCell.java
+++ src/main/java/com/lexyn/pixeldesign/fx/cellfactory/EmitterListCell.java
@@ -1,17 +1,23 @@
 package com.lexyn.pixeldesign.fx.cellfactory;
 
 import com.lexyn.pixeldesign.logic.emitter.Emitter;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.input.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmitterListCell extends ListCell<Emitter> {
+
+    private static final DataFormat emitterFormat = new DataFormat("com.lexyn.pixeldesign.logic.emitter.Emitter");
 
     private final TextField textField = new TextField();
 
     public EmitterListCell() {
+        ListCell<Emitter> thisCell = this;
         textField.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
             if (e.getCode() == KeyCode.ESCAPE) {
                 cancelEdit();
@@ -23,6 +29,9 @@ public class EmitterListCell extends ListCell<Emitter> {
             setContentDisplay(ContentDisplay.TEXT_ONLY);
         });
         setGraphic(textField);
+
+        dragandDrop(thisCell);
+
     }
 
     @Override
@@ -55,6 +64,69 @@ public class EmitterListCell extends ListCell<Emitter> {
         super.cancelEdit();
         setText(getItem().getName());
         setContentDisplay(ContentDisplay.TEXT_ONLY);
+    }
+
+    private void dragandDrop(ListCell<Emitter> thisCell){
+        setOnDragDetected(e -> {
+            if(getItem() == null)
+                return;
+
+            Dragboard dragboard = startDragAndDrop(TransferMode.MOVE);
+            ClipboardContent content = new ClipboardContent();
+            content.put(emitterFormat, getItem());
+            dragboard.setContent(content);
+            e.consume();
+
+        });
+
+        setOnDragOver(e -> {
+            if(e.getGestureSource() != thisCell
+                    && e.getDragboard().hasContent(emitterFormat)) {
+                e.acceptTransferModes(TransferMode.MOVE);
+            }
+            e.consume();
+        });
+
+        setOnDragEntered(e -> {
+            if(e.getGestureSource() != thisCell
+                    && e.getDragboard().hasContent(emitterFormat)) {
+                setOpacity(0.3);
+            }
+            e.consume();
+        });
+
+        setOnDragExited(e -> {
+            if(e.getGestureSource() != thisCell
+                    && e.getDragboard().hasContent(emitterFormat)) {
+                setOpacity(1);
+            }
+        });
+
+        setOnDragDropped(e -> {
+            if(getItem() == null)
+                return;
+
+            Dragboard db = e.getDragboard();
+            boolean success = false;
+
+            if(db.hasContent(emitterFormat)){
+                ObservableList<Emitter> items = getListView().getItems();
+                int draggedIndex = items.indexOf(db.getContent(emitterFormat));
+                int draggedTo = items.indexOf(getItem());
+
+                items.set(draggedIndex, getItem());
+                items.set(draggedTo,(Emitter) db.getContent(emitterFormat));
+
+                List<Emitter> copy = new ArrayList<>(getListView().getItems());
+                getListView().getItems().setAll(copy);
+
+                success = true;
+            }
+            e.setDropCompleted(success);
+            e.consume();
+        });
+
+        setOnDragDone(e -> e.consume());
     }
 
 }
--- src/main/java/com/lexyn/pixeldesign/logic/emitter/Emitter.java
+++ src/main/java/com/lexyn/pixeldesign/logic/emitter/Emitter.java
@@ -1,13 +1,23 @@
 package com.lexyn.pixeldesign.logic.emitter;
 
-public class Emitter {
+import java.io.Serializable;
+
+/**
+ * the main source of particle effect, store and calculated all data relevant for
+ * particles to render
+ * @author lexyna
+ */
+public class Emitter implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String name;
+
+    public Emitter(){}
 
     public Emitter(String name){
         this.name = name;
     }
-
 
     public void setName(String name) {
         this.name = name;
@@ -15,5 +25,18 @@ public class Emitter {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o == this)
+            return true;
+
+        if(!(o instanceof Emitter))
+            return false;
+
+        Emitter copy = (Emitter) o;
+
+        return this.getName().equals(copy.getName());
     }
 }
